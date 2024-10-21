98c5de2308b7f53ae329a91c8c36b0ec594869bc
Entropy1110 <entropy1110@gmail.com>
2024-10-22 01:33:48 +0900

    modified double init script

--- test/inputPoolkey/_check_doubleInit.t.sol
+++ test/inputPoolkey/_check_doubleInit.t.sol
@@ -27,13 +27,14 @@ contract DoubleInitHookTest is Test {
     Currency currency0;
     Currency currency1;
     IPoolManager manager = IPoolManager(0x38EB8B22Df3Ae7fb21e92881151B365Df14ba967);
-    PoolKey key;
+    PoolKey key_real;
+    PoolKey key_fake;
 
     bytes constant ZERO_BYTES = Constants.ZERO_BYTES;
     uint160 constant SQRT_PRICE_1_1 = Constants.SQRT_PRICE_1_1;
 
     function test_double_init() public {
-        string memory code_json = vm.readFile("test/inputPoolkey/doubleInitUnable.json");
+        string memory code_json = vm.readFile("test/inputPoolkey/doubleInitNotYetInitialized.json");
         address _currency0 = vm.parseJsonAddress(code_json, ".data.currency0");
         address _currency1 = vm.parseJsonAddress(code_json, ".data.currency1");
         uint24 _fee = uint24(vm.parseJsonUint(code_json, ".data.fee"));
@@ -42,9 +43,15 @@ contract DoubleInitHookTest is Test {
         currency0 = Currency.wrap(_currency0);
         currency1 = Currency.wrap(_currency1);
 
-        key = PoolKey(currency0, currency1, _fee+1, 60, IHooks(hookAddr));
+        key_real = PoolKey(currency0, currency1, _fee, 60, IHooks(hookAddr));
+        key_fake = PoolKey(currency0, currency1, _fee+1, 60, IHooks(hookAddr));
+        PoolId poolId_real = key_real.toId();
+        (uint160 sqrtPriceX96, int24 tick, uint24 protocolFee, uint24 lpFee) = manager.getSlot0(poolId_real);
 
-        try manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES)
+        if (sqrtPriceX96 == 0) {
+            manager.initialize(key_real, SQRT_PRICE_1_1, ZERO_BYTES);
+        }
+        try manager.initialize(key_fake, SQRT_PRICE_1_1, ZERO_BYTES)
         {
             revert("Double initialization enabled in the hook");
         }catch {
