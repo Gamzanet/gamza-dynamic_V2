1ef64aae298202ec3b908f04d3a70497600ffd06
kenny <xdhgnsx@naver.com>
2025-02-24 20:15:47 +0900

    fix to gas Estimating

--- test/inputPoolkey/hookNoHookCompare.t.sol
+++ test/inputPoolkey/hookNoHookCompare.t.sol
@@ -50,13 +50,20 @@ contract PoolManagerTest is Test, Deployers, setupContract {
 
     PoolKey emptyHook;
     function setUp() public {
-        console.log("setup start :",gasleft());
+        console.log("setup start :", gasleft());
         setupPoolkey();
-        
-        (uint160 sqrtPriceX96,,uint24 pf,uint24 lpfee) = manager.getSlot0(key.toId());
-        (emptyHook,) = initPool(key.currency0, key.currency1, IHooks(address(0)), lpfee , key.tickSpacing, sqrtPriceX96);
-        
-        console.log("setup end:",gasleft());
+
+        (uint160 sqrtPriceX96, , uint24 pf, uint24 lpfee) = manager.getSlot0(key.toId());
+        (emptyHook, ) = initPool(
+            key.currency0,
+            key.currency1,
+            IHooks(address(0)),
+            lpfee,
+            key.tickSpacing,
+            sqrtPriceX96
+        );
+
+        console.log("setup end:", gasleft());
     }
 
     function test_addLiquidity_compare() public {
@@ -69,53 +76,48 @@ contract PoolManagerTest is Test, Deployers, setupContract {
     }
 
     function test_addLiquidity(PoolKey memory keys) internal {
-        if (
-            !Hooks.hasPermission(key.hooks, Hooks.BEFORE_ADD_LIQUIDITY_FLAG) &&
-            !Hooks.hasPermission(key.hooks, Hooks.AFTER_ADD_LIQUIDITY_FLAG)
-        ) {
-            emit log_string("Skip Test");
-            return;
-        }
+        // if (
+        //     !Hooks.hasPermission(key.hooks, Hooks.BEFORE_ADD_LIQUIDITY_FLAG) &&
+        //     !Hooks.hasPermission(key.hooks, Hooks.AFTER_ADD_LIQUIDITY_FLAG)
+        // ) {
+        //     emit log_string("Skip Test");
+        //     return;
+        // }
         uint256 start;
         uint256 end;
-        if (keys.currency0.isAddressZero()) {
-
-            vm.pauseGasMetering();
-            start = gasleft();
-            vm.resumeGasMetering();
-            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
-            vm.pauseGasMetering();
-            end = gasleft();
-            vm.resumeGasMetering();
-            
-        }
-        else {
-            vm.pauseGasMetering();
-            start = gasleft();
-            vm.resumeGasMetering();
-            modifyLiquidityRouter.modifyLiquidity(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
-            vm.pauseGasMetering();
-            end = gasleft();
-            vm.resumeGasMetering();
-
-            
-        }
-        if( address(keys.hooks) == address(0x0) ){
-            console.log("no-hook-add-gas-using : ", (start - end)); 
-            console.log("no-hook-add-gasPrice-using : ", tx.gasprice ); 
-            console.log("no-hook-add-totalGas-using : ", (start - end) * tx.gasprice ); 
-        }
-        else{
-            console.log("hook-add-gas-using : ", (start - end)); 
-            console.log("hook-add-gasPrice-using : ", tx.gasprice ); 
-            console.log("hook-add-totalGas-using : ", (start - end) * tx.gasprice ); 
-        }
         
+        if (keys.currency0.isAddressZero()) {
+            vm.pauseGasMetering();
+            start = gasleft();
+            vm.resumeGasMetering();
 
+            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
+            vm.pauseGasMetering();
+            end = gasleft();
+            vm.resumeGasMetering();
+        } else {
+            vm.pauseGasMetering();
+            start = gasleft();
+            vm.resumeGasMetering();
+
+            modifyLiquidityRouter.modifyLiquidity(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
+            vm.pauseGasMetering();
+            end = gasleft();
+            vm.resumeGasMetering();
+        }
+        if (address(keys.hooks) == address(0x0)) {
+            console.log("no-hook-add-gas-using : ", (start - end));
+            console.log("no-hook-add-gasPrice-using : ", tx.gasprice);
+            console.log("no-hook-add-totalGas-using : ", (start - end) * tx.gasprice);
+        } else {
+            console.log("hook-add-gas-using : ", (start - end));
+            console.log("hook-add-gasPrice-using : ", tx.gasprice);
+            console.log("hook-add-totalGas-using : ", (start - end) * tx.gasprice);
+        }
     }
     function test_removeLiquidity_compare() public {
         vm.startPrank(txOrigin);
-        uint256 snapshot = vm.snapshot();      
+        uint256 snapshot = vm.snapshot();
         test_removeLiquidity(key);
         vm.revertTo(snapshot);
         test_removeLiquidity(emptyHook);
@@ -123,52 +125,49 @@ contract PoolManagerTest is Test, Deployers, setupContract {
     }
 
     function test_removeLiquidity(PoolKey memory keys) internal {
-        if (
-            !Hooks.hasPermission(key.hooks, Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG) &&
-            !Hooks.hasPermission(key.hooks, Hooks.AFTER_REMOVE_LIQUIDITY_FLAG)
-        ) {
-            emit log_string("Skip Test");
-            return;
-        }
+        // if (
+        //     !Hooks.hasPermission(
+        //         key.hooks,
+        //         Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+        //     ) &&
+        //     !Hooks.hasPermission(key.hooks, Hooks.AFTER_REMOVE_LIQUIDITY_FLAG)
+        // ) {
+        //     emit log_string("Skip Test");
+        //     return;
+        // }
         uint256 start;
         uint256 end;
+
         if (keys.currency0.isAddressZero()) {
-            
-            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
-
+            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             start = gasleft();
             vm.resumeGasMetering();
-            modifyLiquidityRouter.modifyLiquidity(keys, REMOVE_LIQUIDITY_PARAMS, ZERO_BYTES);
+            
+            modifyLiquidityRouter.modifyLiquidity(keys, CUSTOM_REMOVE_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             end = gasleft();
             vm.resumeGasMetering();
-
-            
-        }
-        else {
-            modifyLiquidityRouter.modifyLiquidity(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
-
+        } else {
+            modifyLiquidityRouter.modifyLiquidity(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             start = gasleft();
             vm.resumeGasMetering();
-            modifyLiquidityRouter.modifyLiquidity(keys, REMOVE_LIQUIDITY_PARAMS, ZERO_BYTES);
+            
+            modifyLiquidityRouter.modifyLiquidity(keys, CUSTOM_REMOVE_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             end = gasleft();
             vm.resumeGasMetering();
-
         }
-        if( address(keys.hooks) == address(0x0) ){
+        if (address(keys.hooks) == address(0x0)) {
             console.log("no-hook-remove-gas-using : ", (start - end));
-            console.log("no-hook-remove-gasPrice-using : ",tx.gasprice);
+            console.log("no-hook-remove-gasPrice-using : ", tx.gasprice);
             console.log("no-hook-remove-totalGas-using : ", (start - end) * tx.gasprice);
-        }
-        else{
+        } else {
             console.log("hook-remove-gas-using : ", (start - end));
-            console.log("hook-remove-gasPrice-using : ",tx.gasprice);
+            console.log("hook-remove-gasPrice-using : ", tx.gasprice);
             console.log("hook-remove-totalGas-using : ", (start - end) * tx.gasprice);
         }
-        
     }
 
     function test_donate_compare() public {
@@ -180,57 +179,56 @@ contract PoolManagerTest is Test, Deployers, setupContract {
     }
 
     function test_donate(PoolKey memory keys) internal {
-        if (
-            !Hooks.hasPermission(key.hooks, Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG) &&
-            !Hooks.hasPermission(key.hooks, Hooks.AFTER_REMOVE_LIQUIDITY_FLAG)
-        ) {
-            emit log_string("Skip Test");
-            return;
-        }
+        // if (
+        //     !Hooks.hasPermission(
+        //         key.hooks,
+        //         Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+        //     ) &&
+        //     !Hooks.hasPermission(key.hooks, Hooks.AFTER_REMOVE_LIQUIDITY_FLAG)
+        // ) {
+        //     emit log_string("Skip Test");
+        //     return;
+        // }
         uint256 start;
         uint256 end;
-        if (keys.currency0.isAddressZero()){
-            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
 
+        if (keys.currency0.isAddressZero()) {
+            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             start = gasleft();
             vm.resumeGasMetering();
+
             donateRouter.donate{value: 100}(keys, 100, 200, ZERO_BYTES);
             vm.pauseGasMetering();
             end = gasleft();
             vm.resumeGasMetering();
-            
-        }
-        else{
-            modifyLiquidityRouter.modifyLiquidity(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
-
+        } else {
+            modifyLiquidityRouter.modifyLiquidity(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             start = gasleft();
             vm.resumeGasMetering();
+
             donateRouter.donate(keys, 100, 200, ZERO_BYTES);
             vm.pauseGasMetering();
             end = gasleft();
             vm.resumeGasMetering();
-            
         }
 
-        if( address(keys.hooks) == address(0x0) ){
-            console.log("no-hook-donate-gas-using : ", (start - end) );
-            console.log("no-hook-donate-gasPrice-using : ", tx.gasprice );
-            console.log("no-hook-donate-totalGas-using : ", (start - end) * tx.gasprice );
+        if (address(keys.hooks) == address(0x0)) {
+            console.log("no-hook-donate-gas-using : ", (start - end));
+            console.log("no-hook-donate-gasPrice-using : ", tx.gasprice);
+            console.log("no-hook-donate-totalGas-using : ", (start - end) * tx.gasprice);
+        } else {
+            console.log("hook-donate-gas-using : ", (start - end));
+            console.log("hook-donate-gasPrice-using : ", tx.gasprice);
+            console.log("hook-donate-totalGas-using : ", (start - end) * tx.gasprice);
         }
-        else{
-            console.log("hook-donate-gas-using : ", (start - end) );
-            console.log("hook-donate-gasPrice-using : ", tx.gasprice );
-            console.log("hook-donate-totalGas-using : ", (start - end) * tx.gasprice );
-        }
-
     }
 
     function test_swap_compare() public {
         vm.startPrank(txOrigin);
         console.log(gasleft());
-        uint256 snapshot = vm.snapshot();      
+        uint256 snapshot = vm.snapshot();
         test_swap(key);
         vm.revertTo(snapshot);
         test_swap(emptyHook);
@@ -238,65 +236,65 @@ contract PoolManagerTest is Test, Deployers, setupContract {
     }
 
     function test_swap(PoolKey memory keys) internal {
-        if (
-            !Hooks.hasPermission(key.hooks, Hooks.BEFORE_SWAP_FLAG) &&
-            !Hooks.hasPermission(key.hooks, Hooks.AFTER_SWAP_FLAG)
-        ) {
-            emit log_string("Skip Test");
-            return;
-        }
+        // if (
+        //     !Hooks.hasPermission(key.hooks, Hooks.BEFORE_SWAP_FLAG) &&
+        //     !Hooks.hasPermission(key.hooks, Hooks.AFTER_SWAP_FLAG)
+        // ) {
+        //     emit log_string("Skip Test");
+        //     return;
+        // }
         uint256 start;
         uint256 end;
-        
+
         PoolSwapTest.TestSettings memory testSettings = PoolSwapTest.TestSettings({takeClaims: true, settleUsingBurn: false});
-
         BalanceDelta balance;
-        if (key.currency0.isAddressZero()){
-            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
 
-
+        if (key.currency0.isAddressZero()) {
+            modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             start = gasleft();
             vm.resumeGasMetering();
+
             balance = swapRouter.swap{value: 100}(keys, CUSTOM_SWAP_PARAMS, testSettings, ZERO_BYTES);
             vm.pauseGasMetering();
             end = gasleft();
             vm.resumeGasMetering();
-
-        } 
-        else{
-            modifyLiquidityRouter.modifyLiquidity(keys, LIQUIDITY_PARAMS, ZERO_BYTES);
-
+        } else {
+            modifyLiquidityRouter.modifyLiquidity(keys, CUSTOM_LIQUIDITY_PARAMS, ZERO_BYTES);
             vm.pauseGasMetering();
             start = gasleft();
             vm.resumeGasMetering();
+
             balance = swapRouter.swap(keys, CUSTOM_SWAP_PARAMS, testSettings, ZERO_BYTES);
             vm.pauseGasMetering();
             end = gasleft();
             vm.resumeGasMetering();
-
         }
 
-        (uint160 sqrtPrice,int24 tick,uint24 protocolFee,uint24 lpFee ) = manager.getSlot0(keys.toId());
-        if(address(keys.hooks) == address(0x0)){
+        (
+            uint160 sqrtPrice,
+            int24 tick,
+            uint24 protocolFee,
+            uint24 lpFee
+        ) = manager.getSlot0(keys.toId());
+        if (address(keys.hooks) == address(0x0)) {
             console.log("no-hook-Swap-tokenPrice-using : ", sqrtPrice);
             console.log("no-hook-Swap-protocolFee-using : ", protocolFee);
             console.log("no-hook-Swap-lpFee-using : ", lpFee);
-            console.log("no-hook-Swap-balance-amount0-using : ",balance.amount0());
-            console.log("no-hook-Swap-balance-amount1-using : ",balance.amount1());
-            console.log("no-hook-swap-gas-using : ", (start - end) );
-            console.log("no-hook-swap-gasPrice-using : ", tx.gasprice );
-            console.log("no-hook-swap-totalGas-using : ", (start - end) * tx.gasprice );
-        }
-        else{
+            console.log("no-hook-Swap-balance-amount0-using : ", balance.amount0());
+            console.log("no-hook-Swap-balance-amount1-using : ", balance.amount1());
+            console.log("no-hook-swap-gas-using : ", (start - end));
+            console.log("no-hook-swap-gasPrice-using : ", tx.gasprice);
+            console.log("no-hook-swap-totalGas-using : ", (start - end) * tx.gasprice);
+        } else {
             console.log("hook-Swap-tokenPrice-using : ", sqrtPrice);
             console.log("hook-Swap-protocolFee-using : ", protocolFee);
             console.log("hook-Swap-lpFee-using : ", lpFee);
-            console.log("hook-Swap-balance-amount0-using : ",balance.amount0());
-            console.log("hook-Swap-balance-amount1-using : ",balance.amount1());
-            console.log("hook-swap-gas-using : ", (start - end) );
-            console.log("hook-swap-gasPrice-using : ", tx.gasprice );
-            console.log("hook-swap-totalGas-using : ", (start - end) * tx.gasprice );
+            console.log("hook-Swap-balance-amount0-using : ", balance.amount0());
+            console.log("hook-Swap-balance-amount1-using : ", balance.amount1());
+            console.log("hook-swap-gas-using : ", (start - end));
+            console.log("hook-swap-gasPrice-using : ", tx.gasprice);
+            console.log("hook-swap-totalGas-using : ", (start - end) * tx.gasprice);
         }
     }
 }
